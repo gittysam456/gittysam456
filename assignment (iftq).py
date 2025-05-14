995291d6550b06b6852538a852d4d2d27d612f67
gittysam456 <160488866+gittysam456@users.noreply.github.com>
2025-05-14 14:36:58 +0530

    Fix initialization of marks and improve validation logic in Student class

--- assignment (iftq).py
+++ assignment (iftq).py
@@ -125,7 +125,7 @@ for all instance variables to get its values
 class Student:
     def __init__(self):
         self.__student_id = None
-        self.__marks = None
+        self.__marks = 0
         self.__age = None
 
     def set_student_id(self, student_id):
@@ -146,15 +146,15 @@ class Student:
     def get_age(self):
         return self.__age
     def validate_marks(self):
-        if self.__marks>= 0 and self.__marks <=100:
+        if 0 <= self.__marks <= 100:
             return True
         else
             return False
     def validate_age(self):
-        if self.__age >20
+        if self.__age > 20
             return True
         else:
-            False
+           return False
     def check_qualification(self):
         if self.validate_marks() and self.validate_age():
             if self.__marks >= 65:
