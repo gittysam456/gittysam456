a08b6ebcae3729b4bbe7ee680335a6de8f6f6cf8
gittysam456 <160488866+gittysam456@users.noreply.github.com>
2025-05-14 14:35:27 +0530

    Implement Student class with validation methods for admission criteria

--- assignment (iftq).py
+++ assignment (iftq).py
@@ -145,3 +145,21 @@ class Student:
 
     def get_age(self):
         return self.__age
+    def validate_marks(self):
+        if self.__marks>= 0 and self.__marks <=100:
+            return True
+        else
+            return False
+    def validate_age(self):
+        if self.__age >20
+            return True
+        else:
+            False
+    def check_qualification(self):
+        if self.validate_marks() and self.validate_age():
+            if self.__marks >= 65:
+                return True
+            else:
+                return False
+        else:
+            return False
