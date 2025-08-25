8f709233d29516c51df511fce0fdf174c6f8c1b4
gittysam456 <160488866+gittysam456@users.noreply.github.com>
2025-08-25 10:58:49 +0530

    Fix formatting issues and add comments for clarity in data visualization examples

--- matplotnump.py
+++ matplotnump.py
@@ -26,3 +26,28 @@ category_counts=df['Category'].value_counts()
 category_counts.plot(kind='pie',title='Sales Distribution by Category',autopct='%1.1f%%',startangle=90)
 plt.ylabel('') #remove the extra label
 plt.show()
+#plotting two charts sie by side in one window
+dg ,axs = plt.subplots(1,2,figsize=(10,4))
+axs[0].plot(df['sales'],color='blue',linestyle='--')
+axs[0].set_title('Sales')
+axs[1],plot(df['Profit'],color='green',linestyle=':')
+axs[1].set_title('Profit')
+plt.show()
+#multiple plots in one chart window
+fig,axs=plt.subplots(2,2,figsize=(10,10))
+axs[0,0].plot(df['Sales'],color='blue',linestyle='--')
+axs[0,0].set_title('Sales')
+axs[0,1].plot(df['Sales'],color='green',linestyle=':')
+axs[0,1].set_title('Sales')
+axs[1,0].plot(df['Sales'],color='red',linestyle='-.')
+axs[1,0].set_title('Sales')
+axs[1,1].plot(df['Sales'],color='purple',linestyle='-')
+axs[1,1].set_title('Sales')
+plt.show()
+#scatter plot example
+plt.scatter(df['Sales'],df['Profit'],alpha=0.6,c=df['Profit'],cmap='viridis') # cmap is for color combiantion
+plt.title('Scatter plot:sales vs profit')
+plt.xlabel('Sales')
+plt.ylabel('Profit')
+plt.colorbar(label='Profit')
+plt.show()
