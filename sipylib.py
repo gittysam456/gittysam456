49718bf63f73f742a5b7a4e42a2f6ab235115cda
gittysam456 <160488866+gittysam456@users.noreply.github.com>
2025-09-15 10:50:38 +0530

    Add statistical measures documentation in sipylib.py

---
+++ sipylib.py
@@ -0,0 +1,13 @@
+from scipy import stats
+    """measure of the central tendencies
+    - mean : average
+    - median : middle value 
+    - mode : count the most frequent value
+    measures of the dispersion
+    -variance : how far a set of numbers are spread out from their average value
+    -standard deviation : how much variation or dispersion of a set of values
+    - range : difference between the largest and smallest values
+    - quartiles : values that divide a list of numbers into quarters
+    - interquartile range : difference between the first and third quartiles
+    """
+    
